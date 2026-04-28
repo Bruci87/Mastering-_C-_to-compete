5d9aad7083ad4b6c6cbcdf257d35a52fa00cc6e7
Silvio Bruciaferro <116919319+Bruci87@users.noreply.github.com>
2026-04-28 16:08:20 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
+
 int main(){
-    vector<int> array = {1, 2, 3, 5, 7, 9};
-    int n=6, x=5, j=0;
-    for(int i = n/2; i<=1; i/=2){
-        while(j+i < n && array[j+i]<= x)
-        j += i;
-    }
-    if(array[j]=x){
-        cout<<"Achou";
-    }else{
-        cout<<"Nop";
-    }
+    int n = 3;
     
+    for(int b = 0; b < (1<<n);b++){
+        
+        vector<int> subset;
+        
+        for(int i = 0; i < n; i++){
+            if(b&(1<<i)) subset.push_back(i);
+        }
+        
+        cout << "novo: " << "\n";
+        
+        for(auto x:subset){
+            cout << x << "\n";
+        }
+        subset.clear();
+    }
+
+    return 0;
 }
